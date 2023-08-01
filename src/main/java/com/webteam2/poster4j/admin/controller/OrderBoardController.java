021d71e331143f7cb1782f734680a435144bd34f
songwon31 <sws1719@naver.com>
2023-08-02 08:08:19 +0900

    seperate user and admin view

--- src/main/java/com/webteam2/poster4j/admin/controller/OrderBoardController.java
+++ src/main/java/com/webteam2/poster4j/admin/controller/OrderBoardController.java
@@ -1,16 +1,16 @@
-package com.webteam2.poster4j.user.controller;
+package com.webteam2.poster4j.admin.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 @Controller
-public class HomeController {
-	@RequestMapping("/")
+@Slf4j
+@RequestMapping("/admin")
+public class OrderBoardController {
+	@RequestMapping("/orderBoard")
 	public String home() {
-		log.info("실행777");
-		return "user/home";
+		return "admin/orderBoard";
 	}
 }
