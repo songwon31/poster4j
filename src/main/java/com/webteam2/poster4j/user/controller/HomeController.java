80efa2fcc3f6df6058bd365e8a0429d34848965a
hyeonjuleebaek <hyeonjuleebaek@gmail.com>
2023-08-01 22:14:17 +0900

    test2

--- src/main/java/com/webteam2/poster4j/user/controller/HomeController.java
+++ src/main/java/com/webteam2/poster4j/user/controller/HomeController.java
@@ -10,7 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 public class HomeController {
 	@RequestMapping("/")
 	public String home() {
-		log.info("실행5727");
+		log.info("실행1013");
 		return "home";
 	}
 }
