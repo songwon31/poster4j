7420bea0fc658fd7eeb987ed4aeaa15b2c754d37
songwon31 <sws1719@naver.com>
2023-08-07 15:40:10 +0900

    add orderCancel files

---
+++ src/main/java/com/webteam2/poster4j/dao/OrderCancelDao.java
@@ -0,0 +1,14 @@
+package com.webteam2.poster4j.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.webteam2.poster4j.dto.OrderDetail;
+import com.webteam2.poster4j.dto.Pager;
+
+@Mapper
+public interface OrderCancelDao {
+	
+}
---
+++ src/main/java/com/webteam2/poster4j/service/OrderCancelService.java
@@ -0,0 +1,10 @@
+package com.webteam2.poster4j.service;
+
+import java.util.List;
+
+import com.webteam2.poster4j.dto.OrderDetail;
+import com.webteam2.poster4j.dto.Pager;
+
+public interface OrderCancelService {
+	
+}
