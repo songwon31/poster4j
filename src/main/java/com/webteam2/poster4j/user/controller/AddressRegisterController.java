4e18e8f417f916266019a50972e06dac614a3e22
kimfefe <kimcheol8@gmail.com>
2023-08-08 11:43:28 +0900

    receiver.xml

--- src/main/java/com/webteam2/poster4j/user/controller/AddressRegisterController.java
+++ src/main/java/com/webteam2/poster4j/user/controller/AddressRegisterController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.webteam2.poster4j.dto.Customer;
 import com.webteam2.poster4j.dto.Receiver;
 import com.webteam2.poster4j.service.ReceiverService;
 
@@ -28,6 +29,14 @@ public class AddressRegisterController {
 	@PostMapping("/addressRegister")
 	public String AddressRegister(Receiver receiver, HttpSession session) {
 		log.info("배송지 등록 폼 전송");
+		
+		Customer customer = (Customer)session.getAttribute("customerLogin");
+		if (customer == null) {
+			// 로그인 정보가 없으면 로그인 페이지로 이동하거나 필요한 처리를 수행
+			return "redirect:/login"; // 로그인 페이지로 리다이렉트
+		}
+		receiver.setCustomerId(customer.getCustomerId());
+		
 		receiverService.register(receiver);
 		return "redirect:/addressList";
 	}
